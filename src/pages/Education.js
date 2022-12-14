339498c9f7ea501108dd70158f6670daf6b4188e
Olivia Schwartz <ocmooney@gmail.com>
2022-12-14 16:40:42 -0500

    education added

--- src/pages/Education.js
+++ src/pages/Education.js
@@ -2,20 +2,98 @@ const Education = () => {
   return (
     <div className="education-container">
       <h1 className="education-header">Educational Resources</h1>
+      <div className="link=-container"></div>
+      <div className="link-box">
+        <h3 className="Title"></h3>
+        <h4 className="Description"></h4>
+      </div>
+      <div className="link-box">
+        <h3 className="Title"></h3>
+        <h4 className="Description"></h4>
+      </div>
+      <div className="link-box">
+        <h3 className="Title"></h3>
+        <h4 className="Description"></h4>
+      </div>
+
       <div className="education-section">
         <h2 className="section-heading">Understanding Lupus</h2>
+        <div className="link-box">
+          <h3 className="Title"></h3>
+          <h4 className="Description"></h4>
+        </div>
+        <div className="link-box">
+          <h3 className="Title"></h3>
+          <h4 className="Description"></h4>
+        </div>
+        <div className="link-box">
+          <h3 className="Title"></h3>
+          <h4 className="Description"></h4>
+        </div>
       </div>
+
       <div className="education-section">
         <h2 className="section-heading">Diagnosing Lupus</h2>
+        <div className="link-box">
+          <h3 className="Title"></h3>
+          <h4 className="Description"></h4>
+        </div>
+        <div className="link-box">
+          <h3 className="Title"></h3>
+          <h4 className="Description"></h4>
+        </div>
+        <div className="link-box">
+          <h3 className="Title"></h3>
+          <h4 className="Description"></h4>
+        </div>
       </div>
+
       <div className="education-section">
         <h2 className="section-heading">Living with Lupus</h2>
+        <div className="link-box">
+          <h3 className="Title"></h3>
+          <h4 className="Description"></h4>
+        </div>
+        <div className="link-box">
+          <h3 className="Title"></h3>
+          <h4 className="Description"></h4>
+        </div>
+        <div className="link-box">
+          <h3 className="Title"></h3>
+          <h4 className="Description"></h4>
+        </div>
       </div>
+
       <div className="education-section">
         <h2 className="section-heading">Treating Lupus</h2>
+        <div className="link-box">
+          <h3 className="Title"></h3>
+          <h4 className="Description"></h4>
+        </div>
+        <div className="link-box">
+          <h3 className="Title"></h3>
+          <h4 className="Description"></h4>
+        </div>
+        <div className="link-box">
+          <h3 className="Title"></h3>
+          <h4 className="Description"></h4>
+        </div>
       </div>
+
       <div className="education-section">
         <h2 className="section-heading">Financial Resources</h2>
+        <div className="link-box">
+          <h3 className="Title"></h3>
+          <h4 className="Description"></h4>
+        </div>
+        <div className="link-box">
+          <h3 className="Title"></h3>
+          <h4 className="Description"></h4>
+        </div>
+        <div className="link-box">
+          <h3 className="Title"></h3>
+          <h4 className="Description"></h4>
+        </div>
       </div>
     </div>
   )
